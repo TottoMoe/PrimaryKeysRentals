46ba9332ddcc28ac193d2188cff7ffd0f18496ee
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-09-20 21:18:11 -0700

    Added initial code to controllers files

---
+++ controllers/api/reviewRoutes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const { Review } = require('../../models');
+
+module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-undef */
+const router = require('express').Router();
+const { Review, User, Vehicle } = require('../models');
+const withAuth = require('../utils/auth');
+
+module.exports = router;
