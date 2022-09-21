83923cf83f28902b31a1736f6d2c0cfbe0efd51c
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-09-20 20:52:13 -0700

    add public js

---
+++ models/Review.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/config");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Comment",
+  }
+);
+
+module.exports = Comment;
