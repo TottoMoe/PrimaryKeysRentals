96fdce27d7448f6212f97ae185199d8f8b49753f
TottoMoe <105409104+TottoMoe@users.noreply.github.com>
2022-09-22 20:09:03 -0700

    changed all the comments

--- public/js/edit.js
+++ public/js/edit.js
@@ -1,35 +1,35 @@
-// const postId = document.querySelector('input[name="post-id"]').value;
+const postId = document.querySelector('input[name="post-id"]').value;
 
-// const editFormHandler = async function (event) {
-//   event.preventDefault();
+const editFormHandler = async function (event) {
+  event.preventDefault();
 
-//   const title = document.querySelector('input[name="post-title"]').value;
-//   const body = document.querySelector('textarea[name="post-body"]').value;
+  const title = document.querySelector('input[name="post-title"]').value;
+  const body = document.querySelector('textarea[name="post-body"]').value;
 
-//   await fetch(`/api/post/${postId}`, {
-//     method: "PUT",
-//     body: JSON.stringify({
-//       title,
-//       body,
-//     }),
-//     header: {
-//       "Content-Type": "application/json",
-//     },
-//   });
-//   document.location.replace("/dashboard");
-// };
+  await fetch(`/api/post/${postId}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      title,
+      body,
+    }),
+    header: {
+      'Content-Type': 'application/json',
+    },
+  });
+  document.location.replace('/dashboard');
+};
 
-// const deleteClickHandler = async () => {
-//   await fetch(`/api/post/${postId}`, {
-//     method: "DELETE",
-//   });
+const deleteClickHandler = async () => {
+  await fetch(`/api/post/${postId}`, {
+    method: 'DELETE',
+  });
 
-//   document.location.replace("/dashboard");
-// };
+  document.location.replace('/dashboard');
+};
 
-// document
-//   .querySelector("#edit-post-btn")
-//   .addEventListener("submit", editFormHandler);
-// document
-//   .querySelector("delete-btn")
-//   .addEventListener("click", deleteClickHandler);
+document
+  .querySelector('#edit-post-btn')
+  .addEventListener('submit', editFormHandler);
+document
+  .querySelector('delete-btn')
+  .addEventListener('click', deleteClickHandler);
--- public/js/logout.js
+++ public/js/logout.js
@@ -1,14 +1,14 @@
-// const logout = async () => {
-//   const response = await fetch("/api/users/logout", {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//   });
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-//   if (response.ok) {
-//     document.location.replace("/");
-//   } else {
-//     alert("Failed to log out");
-//   }
-// };
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log out');
+  }
+};
 
-// document.querySelector("#logout-link").addEventListener("click", logout);
+document.querySelector('#logout-link').addEventListener('click', logout);
