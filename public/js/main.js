4bbd21f21940a570659c42b3626b0850cde92f0f
Hunter <hunterbrennan15@gmail.com>
2022-09-22 22:25:55 -0400

    linked signup/login buttons to toggle between each page

--- public/js/main.js
+++ public/js/main.js
@@ -1,16 +1,21 @@
-document.addEventListener("DOMContentLoaded", () =>{
-  const loginForm = Document.querySelector("#login");
-  const createAccountForm = Document.querySelector("#createAccount");
 
+document.addEventListener("DOMContentLoaded", () => {
+  const loginForm = document.querySelector("#login");
+  const createAccountForm = document.querySelector("#createAccount");
+  const signUpButton = document.querySelector("#linkCreateAccount");
 
-  document.querySelector("#linkCreateAccount").addEventListener("click", () =>{
-    loginForm.classList.add("form--hidden");
-    createAccountForm.classList.remove("form--hidden");
+ 
+  document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+    e.preventDefault();
+    signUpButton.classList.add("form-hidden");
+    loginForm.classList.add("form-hidden");
+    createAccountForm.classList.remove("form-hidden");
   });
 
 
-  document.querySelector("#linkLogin").addEventListener("click", () =>{
-    loginForm.classList.remove("form--hidden");
-    createAccountForm.classList.add("form--hidden");
+  document.querySelector("#linkLogin").addEventListener("click", e => {
+    e.preventDefault();
+    loginForm.classList.remove("form-hidden");
+    createAccountForm.classList.add("form-hidden");
   });
 });
