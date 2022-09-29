40bdc6d3dfa095235f9ca9b192a1843e0be014df
Hunter <hunterbrennan15@gmail.com>
2022-09-29 18:21:18 -0400

    fixed small styling issues

--- public/js/cart.js
+++ public/js/cart.js
@@ -2,7 +2,40 @@ var removeCartItemButtons = document.getElementsByClassName('btn-remove')
 console.log(removeCartItemButtons)
 for (var i = 0; i < removeCartItemButtons.length; i++) {
   var button = removeCartItemButtons[i]
-  button.addEventListener('click', function(){
-    console.log('clicked')
+  button.addEventListener('click', function(event){
+   var buttonClicked = event.target
+   buttonClicked.parentElement.parentElement.remove()
+   updateCartTotal()
   })
+}
+
+function updateCartTotal(){
+  var cartItemContainer = document.getElementsByClassName('card-display')[0]
+  var cartRows = cartItemContainer.getElementsByClassName('card-row')
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i]
+  }
+}
+
+let generateCartItems = () => {
+  if(basket.length !==0){
+    console.log("basket is not empty")
+  }
+  else{
+    console.log("basket is empty")
+  }
+};
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const cartAdd = document.querySelector('#testCar');
+
+//   document.querySelector('#card-btn').addEventListener('click', (e) => {
+//     e.preventDefault();
+
+//   })
+// })
+
+document.getElementById("#card-btn").onclick = function() {
+  $('#testCar').appendTo('cartBasket');
 }
