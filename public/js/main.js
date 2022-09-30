1efabcf0029152411f6f563ce86e9f81c1141d6a
Hunter <hunterbrennan15@gmail.com>
2022-09-29 21:52:26 -0400

    fixed buttons and other errors

--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
   const loginForm = document.querySelector('#login');
   const createAccountForm = document.querySelector('#createAccount');
   const signUpButton = document.querySelector('#linkCreateAccount');
+  const createAccountBtn = document.querySelector('#createAccountBtn');
 
   document.querySelector('#linkCreateAccount').addEventListener('click', (e) => {
     e.preventDefault();
     signUpButton.classList.add('form-hidden');
     loginForm.classList.add('form-hidden');
     createAccountForm.classList.remove('form-hidden');
+    createAccountBtn.classList.remove('form-hidden');
   });
 
   document.querySelector('#linkLogin').addEventListener('click', (e) => {
